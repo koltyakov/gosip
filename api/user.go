dd979f38a301367eb0daaf8a866e2ded25a599e2
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-11-26 13:32:16 +0300

    User API

---
+++ api/user.go
@@ -0,0 +1,73 @@
+package api
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/koltyakov/gosip"
+)
+
+// User ...
+type User struct {
+	client   *gosip.SPClient
+	conf     *Conf
+	endpoint string
+	oSelect  string
+	oExpand  string
+}
+
+// UserInfo ...
+type UserInfo struct {
+	Email         string `json:"Email"`
+	ID            int    `json:"Id"`
+	IsHiddenInUI  bool   `json:"IsHiddenInUI"`
+	IsSiteAdmin   bool   `json:"IsSiteAdmin"`
+	LoginName     string `json:"LoginName"`
+	PrincipalType int    `json:"PrincipalType"`
+	Title         int    `json:"Title"`
+}
+
+// Conf ...
+func (user *User) Conf(conf *Conf) *User {
+	user.conf = conf
+	return user
+}
+
+// Select ...
+func (user *User) Select(oDataSelect string) *User {
+	user.oSelect = oDataSelect
+	return user
+}
+
+// Expand ...
+func (user *User) Expand(oDataExpand string) *User {
+	user.oExpand = oDataExpand
+	return user
+}
+
+// Get ...
+func (user *User) Get() ([]byte, error) {
+	apiURL, _ := url.Parse(user.endpoint)
+	query := url.Values{}
+	if user.oSelect != "" {
+		query.Add("$select", TrimMultiline(user.oSelect))
+	}
+	if user.oExpand != "" {
+		query.Add("$expand", TrimMultiline(user.oExpand))
+	}
+	apiURL.RawQuery = query.Encode()
+	sp := &HTTPClient{SPClient: user.client}
+	return sp.Get(apiURL.String(), GetConfHeaders(user.conf))
+}
+
+// Groups ...
+func (user *User) Groups() *Groups {
+	return &Groups{
+		client: user.client,
+		conf:   user.conf,
+		endpoint: fmt.Sprintf(
+			"%s/Groups",
+			user.endpoint,
+		),
+	}
+}
