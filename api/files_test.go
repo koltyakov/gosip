602708b9f308108b0f4c972a786ca61ea39c1d96
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2019-11-30 00:13:17 +0300

    Files tests

---
+++ api/files_test.go
@@ -0,0 +1,70 @@
+package api
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestFiles(t *testing.T) {
+	checkClient(t)
+
+	web := NewSP(spClient).Web()
+	newFolderName := uuid.New().String()
+	rootFolderURI := getRelativeURL(spClient.AuthCnfg.GetSiteURL()) + "Shared%20Documents"
+	newFolderURI := rootFolderURI + "/" + newFolderName
+	if _, err := web.GetFolder(rootFolderURI).Folders().Add(newFolderName); err != nil {
+		t.Error(err)
+	}
+
+	t.Run("AddSeries", func(t *testing.T) {
+		for i := 1; i < 10; i++ {
+			fileName := fmt.Sprintf("File_%d.txt", i)
+			fileData := []byte(fmt.Sprintf("File %d data", i))
+			if _, err := web.GetFolder(newFolderURI).Files().Add(fileName, fileData, true); err != nil {
+				t.Error(err)
+			}
+		}
+	})
+
+	t.Run("Get", func(t *testing.T) {
+		if _, err := web.GetFolder(newFolderURI).Files().Get(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("GetByName", func(t *testing.T) {
+		if _, err := web.GetFolder(newFolderURI).Files().GetByName("File_1.txt").Get(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("GetFile", func(t *testing.T) {
+		if _, err := web.GetFile(newFolderURI + "/File_2.txt").Get(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		if _, err := web.GetFolder(newFolderURI).Files().GetByName("File_1.txt").Delete(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Recycle", func(t *testing.T) {
+		if _, err := web.GetFile(newFolderURI + "/File_2.txt").Recycle(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("GetItem", func(t *testing.T) {
+		if _, err := web.GetFile(newFolderURI + "/File_3.txt").Get(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	if _, err := web.GetFolder(rootFolderURI).Delete(); err != nil {
+		t.Error(err)
+	}
+}
