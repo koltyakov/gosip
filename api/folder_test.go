a5c1b223fe86e5a2a84df52c1888af5a48bab1e9
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2019-11-30 00:04:41 +0300

    Folder tests

---
+++ api/folder_test.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestFolder(t *testing.T) {
+	checkClient(t)
+
+	web := NewSP(spClient).Web()
+	newFolderName := uuid.New().String()
+	rootFolderURI := getRelativeURL(spClient.AuthCnfg.GetSiteURL()) + "Shared%20Documents"
+
+	t.Run("Add", func(t *testing.T) {
+		if _, err := web.GetFolder(rootFolderURI).Folders().Add(newFolderName); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Get", func(t *testing.T) {
+		if _, err := web.GetFolder(rootFolderURI + "/" + newFolderName).Get(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("GetItem", func(t *testing.T) {
+		if _, err := web.GetFolder(rootFolderURI + "/" + newFolderName).GetItem(); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Recycle", func(t *testing.T) {
+		if _, err := web.GetFolder(rootFolderURI + "/" + newFolderName).Delete(); err != nil {
+			t.Error(err)
+		}
+		// ToDo: Restore and delete
+	})
+
+}
