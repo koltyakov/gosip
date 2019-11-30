8630dcdfd4c345f06b0c9cf3bb151e0fc1def3c3
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2019-11-30 19:23:57 +0300

    Views API

---
+++ api/view.go
@@ -0,0 +1,126 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+
+	"github.com/koltyakov/gosip"
+)
+
+// View ...
+type View struct {
+	client    *gosip.SPClient
+	config    *RequestConfig
+	endpoint  string
+	modifiers map[string]string
+}
+
+// ViewInfo ...
+type ViewInfo struct {
+	BaseViewID                string `json:"BaseViewId"`
+	DefaultView               bool   `json:"DefaultView"`
+	DefaultViewForContentType bool   `json:"DefaultViewForContentType"`
+	EditorModified            bool   `json:"EditorModified"`
+	Hidden                    bool   `json:"Hidden"`
+	HTMLSchemaXML             string `json:"HtmlSchemaXml"`
+	ID                        string `json:"Id"`
+	ImageURL                  string `json:"ImageUrl"`
+	IncludeRootFolder         bool   `json:"IncludeRootFolder"`
+	JSLink                    string `json:"JSLink"`
+	ListViewXML               string `json:"ListViewXml"`
+	MobileDefaultView         bool   `json:"MobileDefaultView"`
+	MobileView                bool   `json:"MobileView"`
+	OrderedView               bool   `json:"OrderedView"`
+	Paged                     bool   `json:"Paged"`
+	PersonalView              bool   `json:"PersonalView"`
+	ReadOnlyView              bool   `json:"ReadOnlyView"`
+	RequiresClientIntegration bool   `json:"RequiresClientIntegration"`
+	RowLimit                  int    `json:"RowLimit"`
+	Scope                     int    `json:"Scope"`
+	ServerRelativeURL         string `json:"ServerRelativeUrl"`
+	TabularView               bool   `json:"TabularView"`
+	Threaded                  bool   `json:"Threaded"`
+	Title                     string `json:"Title"`
+	ViewQuery                 string `json:"ViewQuery"`
+	ViewType                  string `json:"ViewType"`
+}
+
+// NewView ...
+func NewView(client *gosip.SPClient, endpoint string, config *RequestConfig) *View {
+	return &View{
+		client:   client,
+		endpoint: endpoint,
+		config:   config,
+	}
+}
+
+// ToURL ...
+func (view *View) ToURL() string {
+	apiURL, _ := url.Parse(view.endpoint)
+	query := url.Values{}
+	for k, v := range view.modifiers {
+		query.Add(k, trimMultiline(v))
+	}
+	apiURL.RawQuery = query.Encode()
+	return apiURL.String()
+}
+
+// Conf ...
+func (view *View) Conf(config *RequestConfig) *View {
+	view.config = config
+	return view
+}
+
+// Select ...
+func (view *View) Select(oDataSelect string) *View {
+	if view.modifiers == nil {
+		view.modifiers = make(map[string]string)
+	}
+	view.modifiers["$select"] = oDataSelect
+	return view
+}
+
+// Expand ...
+func (view *View) Expand(oDataExpand string) *View {
+	if view.modifiers == nil {
+		view.modifiers = make(map[string]string)
+	}
+	view.modifiers["$expand"] = oDataExpand
+	return view
+}
+
+// Get ...
+func (view *View) Get() (*ViewInfo, error) {
+	sp := NewHTTPClient(view.client)
+	data, err := sp.Get(view.ToURL(), HeadersPresets.Verbose.Headers)
+	if err != nil {
+		return nil, err
+	}
+	res := &struct {
+		View *ViewInfo `json:"d"`
+	}{}
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, err
+	}
+	return res.View, nil
+}
+
+// GetRaw ...
+func (view *View) GetRaw() ([]byte, error) {
+	sp := NewHTTPClient(view.client)
+	return sp.Get(view.ToURL(), getConfHeaders(view.config))
+}
+
+// Delete ...
+func (view *View) Delete() ([]byte, error) {
+	sp := NewHTTPClient(view.client)
+	return sp.Delete(view.endpoint, getConfHeaders(view.config))
+}
+
+// Recycle ...
+func (view *View) Recycle() ([]byte, error) {
+	sp := NewHTTPClient(view.client)
+	endpoint := fmt.Sprintf("%s/Recycle", view.endpoint)
+	return sp.Post(endpoint, nil, getConfHeaders(view.config))
+}
---
+++ api/views.go
@@ -0,0 +1,121 @@
+package api
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/koltyakov/gosip"
+)
+
+// Views ...
+type Views struct {
+	client    *gosip.SPClient
+	config    *RequestConfig
+	endpoint  string
+	modifiers map[string]string
+}
+
+// NewViews ...
+func NewViews(client *gosip.SPClient, endpoint string, config *RequestConfig) *Views {
+	return &Views{
+		client:   client,
+		endpoint: endpoint,
+		config:   config,
+	}
+}
+
+// ToURL ...
+func (views *Views) ToURL() string {
+	apiURL, _ := url.Parse(views.endpoint)
+	query := url.Values{}
+	for k, v := range views.modifiers {
+		query.Add(k, trimMultiline(v))
+	}
+	apiURL.RawQuery = query.Encode()
+	return apiURL.String()
+}
+
+// Conf ...
+func (views *Views) Conf(config *RequestConfig) *Views {
+	views.config = config
+	return views
+}
+
+// Select ...
+func (views *Views) Select(oDataSelect string) *Views {
+	if views.modifiers == nil {
+		views.modifiers = make(map[string]string)
+	}
+	views.modifiers["$select"] = oDataSelect
+	return views
+}
+
+// Expand ...
+func (views *Views) Expand(oDataExpand string) *Views {
+	if views.modifiers == nil {
+		views.modifiers = make(map[string]string)
+	}
+	views.modifiers["$expand"] = oDataExpand
+	return views
+}
+
+// Filter ...
+func (views *Views) Filter(oDataFilter string) *Views {
+	if views.modifiers == nil {
+		views.modifiers = make(map[string]string)
+	}
+	views.modifiers["$filter"] = oDataFilter
+	return views
+}
+
+// Top ...
+func (views *Views) Top(oDataTop int) *Views {
+	if views.modifiers == nil {
+		views.modifiers = make(map[string]string)
+	}
+	views.modifiers["$top"] = fmt.Sprintf("%d", oDataTop)
+	return views
+}
+
+// OrderBy ...
+func (views *Views) OrderBy(oDataOrderBy string, ascending bool) *Views {
+	direction := "asc"
+	if !ascending {
+		direction = "desc"
+	}
+	if views.modifiers == nil {
+		views.modifiers = make(map[string]string)
+	}
+	if views.modifiers["$orderby"] != "" {
+		views.modifiers["$orderby"] += ","
+	}
+	views.modifiers["$orderby"] += fmt.Sprintf("%s %s", oDataOrderBy, direction)
+	return views
+}
+
+// Get ...
+func (views *Views) Get() ([]byte, error) {
+	sp := NewHTTPClient(views.client)
+	return sp.Get(views.ToURL(), getConfHeaders(views.config))
+}
+
+// GetByID ...
+func (views *Views) GetByID(viewID string) *View {
+	return NewView(
+		views.client,
+		fmt.Sprintf("%s('%s')", views.endpoint, viewID),
+		views.config,
+	)
+}
+
+// GetByTitle ...
+func (views *Views) GetByTitle(title string) *View {
+	return NewView(
+		views.client,
+		fmt.Sprintf("%s/GetByTitle('%s')", views.endpoint, title),
+		views.config,
+	)
+}
+
+// ToDo:
+// Add
