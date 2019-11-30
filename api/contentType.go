4a418ea505e218c9efeb5896c2b5f606972f7f4c
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2019-11-30 18:21:51 +0300

    ContentTypes API

---
+++ api/contentType.go
@@ -0,0 +1,110 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+
+	"github.com/koltyakov/gosip"
+)
+
+// ContentType ...
+type ContentType struct {
+	client    *gosip.SPClient
+	config    *RequestConfig
+	endpoint  string
+	modifiers map[string]string
+}
+
+// ContentTypeInfo ...
+type ContentTypeInfo struct {
+	Description string `json:"Description"`
+	Group       string `json:"Group"`
+	Hidden      bool   `json:"Hidden"`
+	JSLink      string `json:"JSLink"`
+	Name        string `json:"Name"`
+	ReadOnly    bool   `json:"Read"`
+	SchemaXML   string `json:"SchemaXml"`
+	Scope       string `json:"Scope"`
+	Sealed      bool   `json:"Sealed"`
+	ID          string `json:"StringId"`
+}
+
+// NewContentType ...
+func NewContentType(client *gosip.SPClient, endpoint string, config *RequestConfig) *ContentType {
+	return &ContentType{
+		client:   client,
+		endpoint: endpoint,
+		config:   config,
+	}
+}
+
+// ToURL ...
+func (ct *ContentType) ToURL() string {
+	apiURL, _ := url.Parse(ct.endpoint)
+	query := url.Values{}
+	for k, v := range ct.modifiers {
+		query.Add(k, trimMultiline(v))
+	}
+	apiURL.RawQuery = query.Encode()
+	return apiURL.String()
+}
+
+// Conf ...
+func (ct *ContentType) Conf(config *RequestConfig) *ContentType {
+	ct.config = config
+	return ct
+}
+
+// Select ...
+func (ct *ContentType) Select(oDataSelect string) *ContentType {
+	if ct.modifiers == nil {
+		ct.modifiers = make(map[string]string)
+	}
+	ct.modifiers["$select"] = oDataSelect
+	return ct
+}
+
+// Expand ...
+func (ct *ContentType) Expand(oDataExpand string) *ContentType {
+	if ct.modifiers == nil {
+		ct.modifiers = make(map[string]string)
+	}
+	ct.modifiers["$expand"] = oDataExpand
+	return ct
+}
+
+// Get ...
+func (ct *ContentType) Get() (*ContentTypeInfo, error) {
+	sp := NewHTTPClient(ct.client)
+	data, err := sp.Get(ct.ToURL(), HeadersPresets.Verbose.Headers)
+	if err != nil {
+		return nil, err
+	}
+	res := &struct {
+		CT *ContentTypeInfo `json:"d"`
+	}{}
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, err
+	}
+	return res.CT, nil
+}
+
+// GetRaw ...
+func (ct *ContentType) GetRaw() ([]byte, error) {
+	sp := NewHTTPClient(ct.client)
+	return sp.Get(ct.ToURL(), getConfHeaders(ct.config))
+}
+
+// Delete ...
+func (ct *ContentType) Delete() ([]byte, error) {
+	sp := NewHTTPClient(ct.client)
+	return sp.Delete(ct.endpoint, getConfHeaders(ct.config))
+}
+
+// Recycle ...
+func (ct *ContentType) Recycle() ([]byte, error) {
+	sp := NewHTTPClient(ct.client)
+	endpoint := fmt.Sprintf("%s/Recycle", ct.endpoint)
+	return sp.Post(endpoint, nil, getConfHeaders(ct.config))
+}
---
+++ api/contentTypes.go
@@ -0,0 +1,113 @@
+package api
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/koltyakov/gosip"
+)
+
+// ContentTypes ...
+type ContentTypes struct {
+	client    *gosip.SPClient
+	config    *RequestConfig
+	endpoint  string
+	modifiers map[string]string
+}
+
+// NewContentTypes ...
+func NewContentTypes(client *gosip.SPClient, endpoint string, config *RequestConfig) *ContentTypes {
+	return &ContentTypes{
+		client:   client,
+		endpoint: endpoint,
+		config:   config,
+	}
+}
+
+// ToURL ...
+func (contentTypes *ContentTypes) ToURL() string {
+	apiURL, _ := url.Parse(contentTypes.endpoint)
+	query := url.Values{}
+	for k, v := range contentTypes.modifiers {
+		query.Add(k, trimMultiline(v))
+	}
+	apiURL.RawQuery = query.Encode()
+	return apiURL.String()
+}
+
+// Conf ...
+func (contentTypes *ContentTypes) Conf(config *RequestConfig) *ContentTypes {
+	contentTypes.config = config
+	return contentTypes
+}
+
+// Select ...
+func (contentTypes *ContentTypes) Select(oDataSelect string) *ContentTypes {
+	if contentTypes.modifiers == nil {
+		contentTypes.modifiers = make(map[string]string)
+	}
+	contentTypes.modifiers["$select"] = oDataSelect
+	return contentTypes
+}
+
+// Expand ...
+func (contentTypes *ContentTypes) Expand(oDataExpand string) *ContentTypes {
+	if contentTypes.modifiers == nil {
+		contentTypes.modifiers = make(map[string]string)
+	}
+	contentTypes.modifiers["$expand"] = oDataExpand
+	return contentTypes
+}
+
+// Filter ...
+func (contentTypes *ContentTypes) Filter(oDataFilter string) *ContentTypes {
+	if contentTypes.modifiers == nil {
+		contentTypes.modifiers = make(map[string]string)
+	}
+	contentTypes.modifiers["$filter"] = oDataFilter
+	return contentTypes
+}
+
+// Top ...
+func (contentTypes *ContentTypes) Top(oDataTop int) *ContentTypes {
+	if contentTypes.modifiers == nil {
+		contentTypes.modifiers = make(map[string]string)
+	}
+	contentTypes.modifiers["$top"] = fmt.Sprintf("%d", oDataTop)
+	return contentTypes
+}
+
+// OrderBy ...
+func (contentTypes *ContentTypes) OrderBy(oDataOrderBy string, ascending bool) *ContentTypes {
+	direction := "asc"
+	if !ascending {
+		direction = "desc"
+	}
+	if contentTypes.modifiers == nil {
+		contentTypes.modifiers = make(map[string]string)
+	}
+	if contentTypes.modifiers["$orderby"] != "" {
+		contentTypes.modifiers["$orderby"] += ","
+	}
+	contentTypes.modifiers["$orderby"] += fmt.Sprintf("%s %s", oDataOrderBy, direction)
+	return contentTypes
+}
+
+// Get ...
+func (contentTypes *ContentTypes) Get() ([]byte, error) {
+	sp := NewHTTPClient(contentTypes.client)
+	return sp.Get(contentTypes.ToURL(), getConfHeaders(contentTypes.config))
+}
+
+// GetByID ...
+func (contentTypes *ContentTypes) GetByID(contentTypeID string) *ContentType {
+	return NewContentType(
+		contentTypes.client,
+		fmt.Sprintf("%s('%s')", contentTypes.endpoint, contentTypeID),
+		contentTypes.config,
+	)
+}
+
+// ToDo:
+// Add
+// Update
