672510bd8e84e2a43d82f3ac96d89737f38d1ad7
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2019-12-03 11:55:27 +0300

    Site response helper

--- api/site.go
+++ api/site.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/url"
 
@@ -14,6 +15,53 @@ type Site struct {
 	endpoint  string
 	modifiers map[string]string
 }
+
+// SiteInfo ...
+type SiteInfo struct {
+	AllowCreateDeclarativeWorkflow         bool         `json:"AllowCreateDeclarativeWorkflow"`
+	AllowDesigner                          bool         `json:"AllowDesigner"`
+	AllowMasterPageEditing                 bool         `json:"AllowMasterPageEditing"`
+	AllowRevertFromTemplate                bool         `json:"AllowRevertFromTemplate"`
+	AllowSaveDeclarativeWorkflowAsTemplate bool         `json:"AllowSaveDeclarativeWorkflowAsTemplate"`
+	AllowSavePublishDeclarativeWorkflow    bool         `json:"AllowSavePublishDeclarativeWorkflow"`
+	AllowSelfServiceUpgrade                bool         `json:"AllowSelfServiceUpgrade"`
+	AllowSelfServiceUpgradeEvaluation      bool         `json:"AllowSelfServiceUpgradeEvaluation"`
+	AuditLogTrimmingRetention              int          `json:"AuditLogTrimmingRetention"`
+	CompatibilityLevel                     int          `json:"CompatibilityLevel"`
+	CurrentChangeToken                     *StringValue `json:"CurrentChangeToken"`
+	DisableAppViews                        bool         `json:"DisableAppViews"`
+	DisableCompanyWideSharingLinks         bool         `json:"DisableCompanyWideSharingLinks"`
+	DisableFlows                           bool         `json:"DisableFlows"`
+	ExternalSharingTipsEnabled             bool         `json:"ExternalSharingTipsEnabled"`
+	GeoLocation                            string       `json:"GeoLocation"`
+	GroupID                                string       `json:"GroupId"`
+	HubSiteID                              string       `json:"HubSiteId"`
+	ID                                     string       `json:"Id"`
+	IsHubSite                              bool         `json:"IsHubSite"`
+	MaxItemsPerThrottledOperation          int          `json:"MaxItemsPerThrottledOperation"`
+	NeedsB2BUpgrade                        bool         `json:"NeedsB2BUpgrade"`
+	PrimaryURI                             string       `json:"PrimaryUri"`
+	ReadOnly                               bool         `json:"ReadOnly"`
+	RequiredDesignerVersion                string       `json:"RequiredDesignerVersion"`
+	ResourcePath                           *DecodedURL  `json:"ResourcePath"`
+	SandboxedCodeActivationCapability      int          `json:"SandboxedCodeActivationCapability"`
+	SensitivityLabel                       string       `json:"SensitivityLabel"`
+	SensitivityLabelID                     string       `json:"SensitivityLabelId"`
+	ServerRelativeURL                      string       `json:"ServerRelativeUrl"`
+	ShareByEmailEnabled                    bool         `json:"ShareByEmailEnabled"`
+	ShareByLinkEnabled                     bool         `json:"ShareByLinkEnabled"`
+	ShowURLStructure                       bool         `json:"ShowUrlStructure"`
+	TrimAuditLog                           bool         `json:"TrimAuditLog"`
+	UIVersionConfigurationEnabled          bool         `json:"UIVersionConfigurationEnabled"`
+	UpgradeReminderDate                    string       `json:"UpgradeReminderDate"` // time.Time
+	UpgradeScheduled                       bool         `json:"UpgradeScheduled"`
+	UpgradeScheduledDate                   string       `json:"UpgradeScheduledDate"` // time.Time
+	Upgrading                              bool         `json:"Upgrading"`
+	URL                                    string       `json:"Url"`
+}
+
+// SiteResp ...
+type SiteResp []byte
 
 // NewSite ...
 func NewSite(client *gosip.SPClient, endpoint string, config *RequestConfig) *Site {
@@ -60,7 +108,7 @@ func (site *Site) Expand(oDataExpand string) *Site {
 }
 
 // Get ...
-func (site *Site) Get() ([]byte, error) {
+func (site *Site) Get() (SiteResp, error) {
 	sp := NewHTTPClient(site.client)
 	return sp.Get(site.ToURL(), getConfHeaders(site.config))
 }
@@ -78,12 +126,46 @@ func (site *Site) RootWeb() *Web {
 }
 
 // OpenWebByID ...
-func (site *Site) OpenWebByID(webID string) ([]byte, error) {
+func (site *Site) OpenWebByID(webID string) (WebResp, error) {
 	endpoint := fmt.Sprintf("%s/OpenWebById('%s')", site.endpoint, webID)
 	sp := NewHTTPClient(site.client)
 	return sp.Post(endpoint, nil, getConfHeaders(site.config))
 }
 
+// GetChangeToken ...
+func (site *Site) GetChangeToken() (string, error) {
+	data, err := site.Select("CurrentChangeToken").Get()
+	if err != nil {
+		return "", err
+	}
+	return data.Data().CurrentChangeToken.StringValue, nil
+}
+
+// GetChanges ...
+func (site *Site) GetChanges(changeQuery *ChangeQuery) ([]*ChangeInfo, error) {
+	return NewChanges(
+		site.client,
+		fmt.Sprintf("%s/GetChanges", site.endpoint),
+		site.config,
+	).GetChanges(changeQuery)
+}
+
 // ToDo:
 // Features
 // Custom actions
+
+/* Response helpers */
+
+// Data : to get typed data
+func (siteResp *SiteResp) Data() *SiteInfo {
+	data := parseODataItem(*siteResp)
+	res := &SiteInfo{}
+	json.Unmarshal(data, &res)
+	return res
+}
+
+// Unmarshal : to unmarshal to custom object
+func (siteResp *SiteResp) Unmarshal(obj interface{}) error {
+	data := parseODataItem(*siteResp)
+	return json.Unmarshal(data, &obj)
+}
