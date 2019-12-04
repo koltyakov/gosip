a4a43489067ecd7344803c19ec9f254f5a1012ab
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2019-12-04 22:05:14 +0300

    Search tests

---
+++ api/search_test.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"testing"
+)
+
+func TestSearch(t *testing.T) {
+	checkClient(t)
+
+	t.Run("Basic", func(t *testing.T) {
+		sp := NewSP(spClient)
+		_, err := sp.Search().PostQuery(&SearchQuery{
+			QueryText: "*",
+			RowLimit:  10,
+		})
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Unmarshal", func(t *testing.T) {
+		sp := NewSP(spClient)
+		res, err := sp.Search().PostQuery(&SearchQuery{
+			QueryText: "*",
+			RowLimit:  10,
+		})
+		if err != nil {
+			t.Error(err)
+		}
+		if res.Data().ElapsedTime == 0 {
+			t.Error("incorrect response")
+		}
+		if len(res.Data().PrimaryQueryResult.RelevantResults.Table.Rows) == 0 {
+			t.Error("incorrect response")
+		}
+	})
+
+}
