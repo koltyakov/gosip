adb871e8cb684b168179310cf5981a7ec3b7c5e7
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2019-12-08 14:03:52 +0300

    CSOM requests helper

--- api/http.go
+++ api/http.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/koltyakov/gosip"
 )
@@ -151,4 +153,60 @@ func (ctx *HTTPClient) Update(endpoint string, body []byte, headers map[string]s
 	defer resp.Body.Close()
 
 	return ioutil.ReadAll(resp.Body)
+}
+
+// ProcessQuery - CSOM requests helper
+func (ctx *HTTPClient) ProcessQuery(endpoint string, body []byte) ([]byte, error) {
+	if strings.Index(strings.ToLower(endpoint), strings.ToLower("/_vti_bin/client.svc/ProcessQuery")) == -1 {
+		endpoint = fmt.Sprintf("%s/_vti_bin/client.svc/ProcessQuery", strings.Split(endpoint, "/_api")[0])
+	}
+
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(body))
+	if err != nil {
+		return nil, fmt.Errorf("unable to create a request: %v", err)
+	}
+
+	// CSOM headers
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Content-Type", `text/xml;charset="UTF-8"`)
+	req.Header.Add("X-Requested-With", "XMLHttpRequest")
+
+	resp, err := ctx.sp.Execute(req)
+	if err != nil {
+		return nil, fmt.Errorf("unable to request api: %v", err)
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	res := []*struct {
+		SchemaVersion  string `json:"SchemaVersion"`
+		LibraryVersion string `json:"LibraryVersion"`
+		ErrorInfo      *struct {
+			ErrorMessage  string `json:"ErrorMessage"`
+			ErrorValue    string `json:"ErrorValue"`
+			ErrorCode     int    `json:"ErrorCode"`
+			ErrorTypeName string `json:"ErrorTypeName"`
+		} `json:"ErrorInfo"`
+		TraceCorrelationID string `json:"TraceCorrelationId"`
+	}{}
+
+	if err := json.Unmarshal(data, &res); err != nil {
+		return data, err
+	}
+
+	if res[0].ErrorInfo != nil {
+		return data, fmt.Errorf(
+			"%s (Code: %d, %s, Correlation ID: %s)",
+			res[0].ErrorInfo.ErrorMessage,
+			res[0].ErrorInfo.ErrorCode,
+			res[0].ErrorInfo.ErrorTypeName,
+			res[0].TraceCorrelationID,
+		)
+	}
+
+	return data, nil
 }
