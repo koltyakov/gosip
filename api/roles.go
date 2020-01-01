8b8936ee865b7bedffe7c5a6b4d95774b5362840
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2020-01-01 20:24:30 +0300

    Roles methods description

--- api/roles.go
+++ api/roles.go
@@ -44,7 +44,7 @@ func NewRoles(client *gosip.SPClient, endpoint string, config *RequestConfig) *R
 	}
 }
 
-// ResetInheritance ...
+// ResetInheritance resets permissions inheritance for this securable object
 func (permissions *Roles) ResetInheritance() error {
 	sp := NewHTTPClient(permissions.client)
 	endpoint := fmt.Sprintf("%s/ResetRoleInheritance", permissions.endpoint)
@@ -52,7 +52,9 @@ func (permissions *Roles) ResetInheritance() error {
 	return err
 }
 
-// BreakInheritance ...
+// BreakInheritance breaked permissions inheritance for this securable object
+// `copyRoleAssigments` - if true the permissions are copied from the current parent scope
+// `clearSubScopes` - true to make all child securable objects inherit role assignments from the current object
 func (permissions *Roles) BreakInheritance(copyRoleAssigments bool, clearSubScopes bool) error {
 	sp := NewHTTPClient(permissions.client)
 	endpoint := fmt.Sprintf(
@@ -65,7 +67,9 @@ func (permissions *Roles) BreakInheritance(copyRoleAssigments bool, clearSubScop
 	return err
 }
 
-// AddAssigment ...
+// AddAssigment adds role assigment for this securable object. Relevant only for the objects after breaking inheritence.
+// `principalID` - Principal ID - numeric ID from User information list - user or group ID
+// `roleDefID` - Role definition ID, use RoleDefinitions API for getting roleDefID
 func (permissions *Roles) AddAssigment(principalID int, roleDefID int) error {
 	sp := NewHTTPClient(permissions.client)
 	endpoint := fmt.Sprintf(
@@ -78,7 +82,9 @@ func (permissions *Roles) AddAssigment(principalID int, roleDefID int) error {
 	return err
 }
 
-// RemoveAssigment ...
+// RemoveAssigment removes specified role assigment for a principal for this securable object.
+// `principalID` - Principal ID - numeric ID from User information list - user or group ID
+// `roleDefID` - Role definition ID, use RoleDefinitions API for getting roleDefID
 func (permissions *Roles) RemoveAssigment(principalID int, roleDefID int) error {
 	sp := NewHTTPClient(permissions.client)
 	endpoint := fmt.Sprintf(
