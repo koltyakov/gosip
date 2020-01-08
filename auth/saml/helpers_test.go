9c2cc92ffaca5cf637955808b0883c43db1eb4f8
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2020-01-08 19:00:34 +0300

    Auth formal coverage improvement

--- auth/saml/helpers_test.go
+++ auth/saml/helpers_test.go
@@ -13,4 +13,14 @@ func TestHelpersEdgeCases(t *testing.T) {
 		}
 	})
 
+	t.Run("getSecurityTokenWithAdfs", func(t *testing.T) {
+		cnfg := &AuthCnfg{SiteURL: ""}
+		if _, _, err := getSecurityTokenWithAdfs("wrong", cnfg); err == nil {
+			t.Error("wrong adfsURL should not go")
+		}
+		if _, _, err := getSecurityTokenWithAdfs("http://wrong", cnfg); err == nil {
+			t.Error("wrong adfsURL should not go")
+		}
+	})
+
 }
