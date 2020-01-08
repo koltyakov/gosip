c93fc1477d21d1858e0570e74c960024b3e22b48
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2020-01-08 16:21:52 +0300

    Minor

--- api/file_test.go
+++ api/file_test.go
@@ -32,7 +32,7 @@ func TestFile(t *testing.T) {
 	t.Run("Modifiers", func(t *testing.T) {
 		f := web.GetFolder(newFolderURI).Files().GetByName("File_1.txt")
 		mods := f.Select("*").Expand("*").modifiers
-		if mods == nil || len(mods.mods) != 5 {
+		if mods == nil || len(mods.mods) != 2 {
 			t.Error("can't add modifiers")
 		}
 	})
