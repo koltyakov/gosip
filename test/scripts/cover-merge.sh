a566237d71199e8d83857505715aac2d9cabc75b
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2020-01-08 17:52:50 +0300

    Coverage script update

--- test/scripts/cover-merge.sh
+++ test/scripts/cover-merge.sh
@@ -1,21 +1,30 @@
-echo "" > coverage.txt
+echo "mode: atomic" > coverage.txt
 
 # Locally precovered strategies
 strategies=( addin adfs fba ntml saml tmg )
 for strategy in "${strategies[@]}"
 do
   auth_coverage_file="auth/${strategy}/coverage.data"
-  if [ -f auth_coverage_file ]; then
-    cat auth_coverage_file >> coverage.txt
+  echo $auth_coverage_file
+  if [ -f $auth_coverage_file ]; then
+    cat $auth_coverage_file \
+      | egrep -v '^mode.*' \
+      >> coverage.txt
   fi
 done
 
 if [ -f auth_coverage.out ]; then
-  cat auth_coverage.out >> coverage.txt
+  cat auth_coverage.out \
+    | egrep -v '^mode.*' \
+    | egrep -v '^github.com/koltyakov/gosip/auth/.*' \
+    | egrep -v '^github.com/koltyakov/gosip/api/.*' \
+    >> coverage.txt
   rm auth_coverage.out
 fi
 
 if [ -f api_coverage.out ]; then
-  cat api_coverage.out >> coverage.txt
+  cat api_coverage.out \
+    | egrep -v '^mode.*' \
+    >> coverage.txt
   rm api_coverage.out
 fi
