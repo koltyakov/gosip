5c3d54aa330b9be869def4ad3660ee9c192e9d48
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2020-01-25 23:21:49 +0300

    Delete sP_gen.go

--- api/sP_gen.go
+++
@@ -1,8 +0,0 @@
-// Package api :: This is auto generated file, do not edit manually
-package api
-
-// Conf receives custom request config definition, e.g. custom headers, custom OData mod
-func (sP *SP) Conf(config *RequestConfig) *SP {
-	sP.config = config
-	return sP
-}
