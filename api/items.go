8a0b9847f276ad4eebb5425f58b75e79e30be82d
ARVO\andrew.koltyakov <andrew.koltyakov@arvosys.com>
2020-02-06 16:55:05 +0300

    Paged collections generator

--- api/items.go
+++ api/items.go
@@ -11,7 +11,7 @@ import (
 	"github.com/koltyakov/gosip"
 )
 
-//go:generate ggen -ent Items -item Item -conf -coll -mods Select,Expand,Filter,Top,Skip,OrderBy -helpers Data,Normalized
+//go:generate ggen -ent Items -item Item -conf -coll -mods Select,Expand,Filter,Top,Skip,OrderBy -helpers Data,Normalized,Pagination
 
 // Items represent SharePoint Lists & Document Libraries Items API queryable collection struct
 // Always use NewItems constructor instead of &Items{}
@@ -24,13 +24,6 @@ type Items struct {
 
 // ItemsResp - items response type with helper processor methods
 type ItemsResp []byte
-
-// ItemsPage - paged items
-type ItemsPage struct {
-	Items       ItemsResp
-	HasNextPage func() bool
-	GetNextPage func() (*ItemsPage, error)
-}
 
 // NewItems - Items struct constructor function
 func NewItems(client *gosip.SPClient, endpoint string, config *RequestConfig) *Items {
@@ -57,65 +50,10 @@ func (items *Items) Get() (ItemsResp, error) {
 	return data, nil
 }
 
-// GetPaged gets Paged Items collection
-func (items *Items) GetPaged() (*ItemsPage, error) {
-	data, err := items.Get()
-	if err != nil {
-		return nil, err
-	}
-	res := &ItemsPage{
-		Items: data,
-		HasNextPage: func() bool {
-			return data.HasNextPage()
-		},
-		GetNextPage: func() (*ItemsPage, error) {
-			nextURL := data.NextPageURL()
-			if nextURL == "" {
-				return nil, fmt.Errorf("unable to get next page")
-			}
-			return NewItems(items.client, nextURL, items.config).GetPaged()
-		},
-	}
-	return res, nil
-}
-
 // GetAll gets all items in a list using internal page helper. The use case of the method is getting all the content from large lists.
 // Method ignores custom sorting and filtering as not supported for the large lists due to throttling limitations.
 func (items *Items) GetAll() ([]ItemResp, error) {
 	return getAll(nil, nil, items)
-}
-
-func getAll(res []ItemResp, cur ItemsResp, items *Items) ([]ItemResp, error) {
-	if res == nil && cur == nil {
-		itemsCopy := NewItems(items.client, items.endpoint, items.config)
-		for key, val := range items.modifiers.Get() {
-			switch key {
-			case "$select":
-				itemsCopy.modifiers.AddSelect(val)
-			case "$expand":
-				itemsCopy.modifiers.AddExpand(val)
-			case "$top":
-				top, _ := strconv.Atoi(val)
-				itemsCopy.modifiers.AddTop(top)
-			}
-		}
-		itemsResp, err := itemsCopy.Get()
-		if err != nil {
-			return nil, err
-		}
-		res = itemsResp.Data()
-		cur = itemsResp
-	}
-	nextURL := getODataCollectionNextPageURL(cur)
-	if nextURL == "" {
-		return res, nil
-	}
-	nextItemsResp, err := NewItems(items.client, nextURL, items.config).Get()
-	if err != nil {
-		return res, err
-	}
-	res = append(res, nextItemsResp.Data()...)
-	return getAll(res, nextItemsResp, items)
 }
 
 // Add adds new item in this list. `body` parameter is byte array representation of JSON string payload relevalt to item metadata object.
@@ -170,14 +108,37 @@ func (items *Items) GetByCAML(caml string) (ItemsResp, error) {
 // ToDo:
 // Batch
 
-/* Response helpers */
+// Helper methods
 
-// NextPageURL : gets next page OData collection
-func (itemsResp *ItemsResp) NextPageURL() string {
-	return getODataCollectionNextPageURL(*itemsResp)
-}
-
-// HasNextPage : returns is true if next page exists
-func (itemsResp *ItemsResp) HasNextPage() bool {
-	return itemsResp.NextPageURL() != ""
+func getAll(res []ItemResp, cur ItemsResp, items *Items) ([]ItemResp, error) {
+	if res == nil && cur == nil {
+		itemsCopy := NewItems(items.client, items.endpoint, items.config)
+		for key, val := range items.modifiers.Get() {
+			switch key {
+			case "$select":
+				itemsCopy.modifiers.AddSelect(val)
+			case "$expand":
+				itemsCopy.modifiers.AddExpand(val)
+			case "$top":
+				top, _ := strconv.Atoi(val)
+				itemsCopy.modifiers.AddTop(top)
+			}
+		}
+		itemsResp, err := itemsCopy.Get()
+		if err != nil {
+			return nil, err
+		}
+		res = itemsResp.Data()
+		cur = itemsResp
+	}
+	nextURL := getODataCollectionNextPageURL(cur)
+	if nextURL == "" {
+		return res, nil
+	}
+	nextItemsResp, err := NewItems(items.client, nextURL, items.config).Get()
+	if err != nil {
+		return res, err
+	}
+	res = append(res, nextItemsResp.Data()...)
+	return getAll(res, nextItemsResp, items)
 }
