8b017f5a360a2726e2978179a9116c8a3e6853e1
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2023-02-12 15:12:59 -0600

    Azure auth

---
+++ auth/azure/azure_test.go
@@ -0,0 +1,90 @@
+package azure
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	h "github.com/koltyakov/gosip/test/helpers"
+	u "github.com/koltyakov/gosip/test/utils"
+)
+
+var cnfgPath = "./config/private.azure.json"
+
+func TestGettingAuthToken(t *testing.T) {
+	if !h.ConfigExists(cnfgPath) {
+		t.Skip("No auth config provided")
+	}
+	err := h.CheckAuth(
+		&AuthCnfg{},
+		cnfgPath,
+		[]string{"SiteURL", "TenantID", "ClientID", "CertPath", "CertPass"},
+	)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestGettingDigest(t *testing.T) {
+	if !h.ConfigExists(cnfgPath) {
+		t.Skip("No auth config provided")
+	}
+	err := h.CheckDigest(&AuthCnfg{}, cnfgPath)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRequest(t *testing.T) {
+	if !h.ConfigExists(cnfgPath) {
+		t.Skip("No auth config provided")
+	}
+	err := h.CheckRequest(&AuthCnfg{}, cnfgPath)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestAuthEdgeCases(t *testing.T) {
+	t.Run("ReadConfig/MissedConfig", func(t *testing.T) {
+		cnfg := &AuthCnfg{}
+		if err := cnfg.ReadConfig("wrong_path.json"); err == nil {
+			t.Error("wrong_path config should not pass")
+		}
+	})
+
+	t.Run("ReadConfig/MissedConfig", func(t *testing.T) {
+		cnfg := &AuthCnfg{}
+		folderPath := u.ResolveCnfgPath("./tmp")
+		filePath := u.ResolveCnfgPath("./tmp/private.azurecert.malformed.json")
+		_ = os.MkdirAll(folderPath, os.ModePerm)
+		_ = ioutil.WriteFile(filePath, []byte("not a json"), 0644)
+		if err := cnfg.ReadConfig(filePath); err == nil {
+			t.Error("malformed config should not pass")
+		}
+		_ = os.RemoveAll(filePath)
+	})
+
+	t.Run("WriteConfig", func(t *testing.T) {
+		folderPath := u.ResolveCnfgPath("./tmp")
+		filePath := u.ResolveCnfgPath("./tmp/private.azurecert.json")
+		cnfg := &AuthCnfg{
+			SiteURL: "test",
+		}
+		_ = os.MkdirAll(folderPath, os.ModePerm)
+		if err := cnfg.WriteConfig(filePath); err != nil {
+			t.Error(err)
+		}
+		_ = os.RemoveAll(filePath)
+	})
+}
+
+func TestCheckTransport(t *testing.T) {
+	if !h.ConfigExists(cnfgPath) {
+		t.Skip("No auth config provided")
+	}
+	err := h.CheckTransport(&AuthCnfg{}, cnfgPath)
+	if err != nil {
+		t.Error(err)
+	}
+}
